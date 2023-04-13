68c4dc6f74b211053a1b9c71c1d7b79368ee8ccb
Nicolas Failla <nfailla93@gmail.com>
2023-04-12 22:19:44 -0300

    ej 2f

--- 2.hs
+++ 2.hs
@@ -46,6 +46,16 @@ ambosSon0' _ _ = False
 
 -- f) mismoIntervalo: dados dos números reales, indica si están relacionados considerando la relación de equivalencia en R
 -- cuyas clases de equivalencia son: (−∞, 3],(3, 7] y (7, ∞), o dicho de otra forma, si pertenecen al mismo intervalo.
+mismoIntervalo :: Float -> Float -> Bool
+mismoIntervalo a b = (a <= 3 && b <= 3) || (a > 3 && b > 3 && a <= 7 && b <= 7) || (a > 7 && b > 7)
+
+mismoIntervalo' :: Float -> Float -> Bool
+mismoIntervalo' a b
+  | a <= 3 && b <= 3 = True
+  | a > 3 && b > 3 && a <= 7 && b <= 7 = True
+  | a > 7 && b > 7 = True
+  | otherwise = False
+
 -- g) sumaDistintos: que dados tres números enteros calcule la suma sin sumar repetidos (si los hubiera).
 -- h) esMultiploDe: dados dos números naturales, decidir si el primero es múltiplo del segundo.
 -- i) digitoUnidades: dado un número natural, extrae su dígito de las unidades.
