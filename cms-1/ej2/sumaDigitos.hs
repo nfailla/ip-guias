fdeaa18822859bd2812ec123d1b2eb065c6c1291
Nicolas Failla <nfailla93@gmail.com>
2023-04-21 21:19:34 -0300

    cms-1 ej2

--- cms-1/ej2/sumaDigitos.hs
+++ cms-1/ej2/sumaDigitos.hs
@@ -1,11 +1,18 @@
 -- No editar esta parte
-main :: IO()
-main = do {
-  x <- readLn ;
-  print(sumaDigitos(x ::(Int)))
-  }
+main :: IO ()
+main = do
+  x <- readLn
+  print (sumaDigitos (x :: (Int)))
 
 sumaDigitos :: Int -> Int
 -- Completar la definición de la función
+sumaDigitos n
+  | n < 10 = n
+  | otherwise = ultimoDigito n + sumaDigitos (sinUltimoDigito n)
 
 -- Pueden agregan las funciones que consideren necesarias
+ultimoDigito :: Int -> Int
+ultimoDigito n = n `mod` 10
+
+sinUltimoDigito :: Int -> Int
+sinUltimoDigito n = n `div` 10
