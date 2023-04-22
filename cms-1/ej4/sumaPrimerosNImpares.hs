4ceb6e1b1e155e14dfeffe6078a100bdbe456c4f
Nicolas Failla <nfailla93@gmail.com>
2023-04-21 21:49:28 -0300

    cms-1 ej4

--- cms-1/ej4/sumaPrimerosNImpares.hs
+++ cms-1/ej4/sumaPrimerosNImpares.hs
@@ -1,13 +1,27 @@
 -- No editar esta parte
-main :: IO()
-main = do {
-  x <- readLn ;
-  print(sumaPrimerosNImpares(x ::(Integer)))
-  }
+main :: IO ()
+main = do
+  x <- readLn
+  print (sumaPrimerosNImpares (x :: (Integer)))
 
 sumaPrimerosNImpares :: Integer -> Integer
 -- Completar la definición de la función
+-- sumaPrimerosNImpares 1 = 0
+-- sumaPrimerosNImpares n = sumaPrimerosNImparesAux (2 * n) - 1
+sumaPrimerosNImpares 1 = 0
+sumaPrimerosNImpares n
+  | hasta < 1 = 0
+  | otherwise = sumaPrimerosNImparesAux hasta
+  where
+    hasta = (2 * n) - 1
 
 -- Pueden agregan las funciones que consideren necesarias
 
+sumaPrimerosNImparesAux :: Integer -> Integer
+sumaPrimerosNImparesAux 1 = 2 * 1 + 2
+sumaPrimerosNImparesAux i
+  | esPar i = 0
+  | otherwise = ((2 * i) + 2) + sumaPrimerosNImparesAux (i - 1)
 
+esPar :: Integer -> Bool
+esPar a = a `mod` 2 == 0
