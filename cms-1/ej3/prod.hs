e44fafdb3417cacd103ccb3744f19b4a3dfefc17
Nicolas Failla <nfailla93@gmail.com>
2023-04-21 21:33:49 -0300

    cms-1 ej3

--- cms-1/ej3/prod.hs
+++ cms-1/ej3/prod.hs
@@ -1,11 +1,14 @@
 -- No editar esta parte
-main :: IO()
-main = do {
-  x <- readLn ;
-  print(prod(x ::(Integer)))
-  }
+main :: IO ()
+main = do
+  x <- readLn
+  print (prod (x :: (Integer)))
 
 prod :: Integer -> Integer
 -- Completar la definición de la función
+prod n = prodAux (2 * n)
 
 -- Pueden agregan las funciones que consideren necesarias
+prodAux :: Integer -> Integer
+prodAux 1 = (1 ^ 2) + (2 * 1)
+prodAux i = ((i ^ 2) + (2 * i)) * prodAux (i - 1)
