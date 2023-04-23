46bb64610cb73415c31823c602ddc82b6049ff42
Nicolas Failla <nfailla93@gmail.com>
2023-04-23 12:25:34 -0300

    cms-1 ej5

--- cms-1/ej5/combinacionesMenoresOIguales.hs
+++ cms-1/ej5/combinacionesMenoresOIguales.hs
@@ -1,13 +1,18 @@
 -- No editar esta parte
-main :: IO()
-main = do {
-  x <- readLn ;
-  print(combinacionesMenoresOiguales(x ::(Integer)))
-  }
+main :: IO ()
+main = do
+  x <- readLn
+  print (combinacionesMenoresOiguales (x :: (Integer)))
 
 combinacionesMenoresOiguales :: Integer -> Integer
 -- Completar la definición de la función
+combinacionesMenoresOiguales n = aux n n n
 
 -- Pueden agregan las funciones que consideren necesarias
+aux :: Integer -> Integer -> Integer -> Integer
+aux n 1 1 = sumSiPred n 1 1
+aux n 1 j = sumSiPred n 1 j + aux n n (j - 1)
+aux n i j = sumSiPred n i j + aux n (i - 1) j
 
-
+sumSiPred :: Integer -> Integer -> Integer -> Integer
+sumSiPred n i j = if (i * j) <= n then 1 else 0
