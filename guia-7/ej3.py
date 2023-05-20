87d1e599928e23c00015b3df566a59bb33dd1dfe
Nicolas Failla <nfailla93@gmail.com>
2023-05-20 02:16:03 -0300

    add guia-7 ej3

---
+++ guia-7/ej3.py
@@ -0,0 +1,16 @@
+
+def alguno_es_0(numero1:float,numero2:float) -> bool:
+  return numero1 == 0 or numero2 == 0
+
+def ambos_son_0(numero1:float,numero2:float) -> bool:
+  return numero1 == 0 and numero2 == 0
+
+def es_nombre_largo(nombre: str) -> bool:
+  length = len(nombre)
+  return length >= 3 and length <= 8
+
+def es_bisiesto(anio: int) -> bool:
+  return es_multiplo_de(anio, 400) or (es_multiplo_de(anio, 4) and not es_multiplo_de(anio, 100))
+
+def es_multiplo_de(n: int, m: int) -> bool:
+  return n % m == 0
