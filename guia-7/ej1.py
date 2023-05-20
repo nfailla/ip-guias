cccbb0a9e53fbb050506a26dd3d2d1de27c7510a
Nicolas Failla <nfailla93@gmail.com>
2023-05-20 02:01:50 -0300

    add guia-7 ej1

---
+++ guia-7/ej1.py
@@ -0,0 +1,33 @@
+from math import sqrt
+
+def raizDe2() -> float:
+  return round(sqrt(2), 4)
+
+def imprimir_hola() -> None:
+  print("hola")
+
+def imprimir_un_verso() -> None:
+  print("Fui a Vandalucía") 
+  print("Donde su pueblo está")
+  print("No tardé en sentirme")
+  print("Un luchador total")
+
+def factorial_de_dos() -> int:
+  return factorial_de(2)
+
+def factorial_de_tres() -> int:
+  return factorial_de(3)
+
+def factorial_de_cuatro() -> int:
+  return factorial_de(4)
+
+def factorial_de_cinco() -> int:
+  return factorial_de(5)
+
+
+def factorial_de(n: int) -> int:
+  res = 1
+  for i in range(1, n):
+    res *= i
+  return res
+
