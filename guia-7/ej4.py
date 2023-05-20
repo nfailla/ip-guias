6db3bb8c9dcc7d322f83f33bce2a5ba5a38ee99f
Nicolas Failla <nfailla93@gmail.com>
2023-05-20 02:39:21 -0300

    update guia-7 ej4

--- guia-7/ej4.py
+++ guia-7/ej4.py
@@ -1,14 +1,15 @@
 def peso_pino(altura_en_metros: float) -> float:
   altura_en_centimetros = altura_en_metros * 100
 
-  if altura_en_centimetros > 3000:
-    altura_sobre_3m_en_cm = altura_en_centimetros - 3000
-    return altura_en_centimetros * 3 + altura_sobre_3m_en_cm * 2
+  if max(altura_en_centimetros, 3000) == 3000:
+    return altura_en_centimetros * 3
+
+  altura_sobre_3m_en_cm = altura_en_centimetros - 3000
+  return altura_en_centimetros * 3 + altura_sobre_3m_en_cm * 2
   
-  return altura_en_centimetros * 3
 
 def es_peso_util(peso_en_kg: float) -> bool:
-  return peso_en_kg >= 400 and peso_en_kg <= 1000
+  return peso_en_kg == max(peso_en_kg, 400) and peso_en_kg == min(peso_en_kg, 1000)
 
 def sirve_pino(altura_en_metros: float) -> bool:
   return es_peso_util(peso_pino(altura_en_metros))
